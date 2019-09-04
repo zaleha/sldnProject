52907ad97c06f1c0b56fca42eebf5ce812896151
Lenovo-PC\Lenovo <pentadbirdbp@gmail.com>
2019-09-04 15:29:35 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ilandskap";
+$dbname = "db_sldn";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
